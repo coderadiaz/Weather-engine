768a349415f60ece8894afbf08985a3fa103b428
coderadiaz <alediazlimardo@gmail.com>
2023-09-10 22:50:18 -0400

    show current climate characteristics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 function displayTemp(response) {
-  console.log(response.data.temperature.current);
+  console.log(response);
+  let temperatureElement = document.querySelector(`#temp-number`);
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector(`#city-name`);
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector(`#description`);
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector(`#humidity`);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windSpeedElement = document.querySelector(`#wind`);
+  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+  let precipitationElement = document.querySelector(`#precipitation`);
 }
+
 let city = `Madrid`;
 let apiKey = `da56a69ff3085ect3555f3472e44ofab`;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
