a35ebd355001511e3d1867c29ced134ccff0f549
coderadiaz <alediazlimardo@gmail.com>
2023-09-11 03:11:39 -0400

    Updated the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    `Sunday`,
+    `Monday`,
+    `Wednesday`,
+    `Thursday`,
+    `Friday`,
+    `Saturday`,
+    `Sunday`,
+  ];
+  let day = days[date.getDay()];
+  return `Last updated: ${day} ${hours}:${minutes}`;
+}
 function displayTemp(response) {
   console.log(response);
   let temperatureElement = document.querySelector(`#temp-number`);
@@ -10,7 +32,8 @@ function displayTemp(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   let windSpeedElement = document.querySelector(`#wind`);
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
-  let precipitationElement = document.querySelector(`#precipitation`);
+  let dateElement = document.querySelector(`#date`);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
 }
 
 let city = `Madrid`;
