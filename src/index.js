ec560681059f1e648e86e0c7fb99280604aeffeb
coderadiaz <alediazlimardo@gmail.com>
2023-09-10 20:37:02 -0400

    API installing
    Added Axios, the API key and the API URL

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+function displayTemp(response) {
+  console.log(response.data.temperature.current);
+}
+let city = `Madrid`;
+let apiKey = `da56a69ff3085ect3555f3472e44ofab`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
+axios.get(apiUrl).then(displayTemp);
